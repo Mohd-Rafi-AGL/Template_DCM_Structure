d5188d31f5d73e14e4dc31d9147f4aa07aa1517a
Mohd Rafi <mohd.rafi@adglobal360.com>
2023-06-12 15:56:42 +0530

    dcm theme used

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import image from '@astrojs/image';
+
+export default defineConfig({
+  // ...
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
+})
